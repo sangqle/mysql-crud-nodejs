3d8e056bc0b037d5517f5067e4a8514caeb4ed06
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2018-10-20 07:04:44 +0000

    liu liu

---
+++ client/src/components/Login.js
@@ -0,0 +1,49 @@
+// eslint-disable-next-line
+import React, { Component } from "react";
+import {
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
+import "./login.css";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Container className="App">
+        <h2>Sign In</h2>
+        <Form className="form">
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="myemail@email.com"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default LoginForm;
