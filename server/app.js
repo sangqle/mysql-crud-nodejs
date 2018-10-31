cb84fa828f7b87a69d903ec6ba363d9f5923fafd
lequangsang97 <sang.lequang97@gmail.com>
2018-10-31 15:59:07 +0700

    Fix json web token instead session

--- server/app.js
+++ server/app.js
@@ -5,10 +5,14 @@ const uuid = require('uuid/v4');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const {authentication} = require('./src/middleware/authentication');
 const { pool } = require('./src/mysql/connect');
 const userController = require("./src/controller/user");
 const adminController = require("./src/controller/admin");
 
+const App = express();
+App.use(bodyParser.json());
+App.use(bodyParser.urlencoded({ extended: true }));
 // Add headers
 App.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -22,91 +26,39 @@ App.use(function (req, res, next) {
   next();
 });
 
-App.get('/user/login', (req, res) => {
-  console.log('Inside GET /user/login callback function')
-  console.log(req.sessionID);
-  res.send(`You got the User login page!\n`)
-});
-
-App.post('/user/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (info) { return res.send(info.message) }
-    if (err) { return next(err); }
-    if (!user) { return res.redirect('/user/login'); }
-    console.log('Inside passport.authenticate() Thanh Cong');
-    req.login(user, (err) => {
-      return res.send(user);
-    })
-  })(req, res, next);
-});
 
 App.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
+  res.redirect('you hit logout page');
 });
 
-App.get('/authrequired', (req, res) => {
-  console.log('Inside GET /authrequired callback')
-  console.log(`User authenticated? ${req.isAuthenticated()}`)
-  if (req.isAuthenticated()) {
-    res.send('you hit the authentication endpoint\n');
+App.get('/secure', authentication, (req, res) => {
+  if(req.user.role === 'admin') {
+    res.send('you are in authrequired page');
   } else {
-    res.redirect('/')
+    res.send('please try as admin again!')
   }
 });
 
-// Route
 App.post("/user/create/account", userController.addAccount); // ok.
+App.get('/user/login', userController.userGetLogin);
+App.post('/user/login', userController.userPostLogin);
+
 App.get("/user/get/all/movie", userController.getAllMovie); // ok
 App.get("/user/get/date/:id_movie", userController.getAllDateOfMovie); // ok
 App.get("/user/get/time/:id_movie/:id_date", userController.getAllTimeOfDateInMovie); // ok
 App.get("/user/get/seated/:id_movie/:id_date/:id_time", userController.getChoNgoiDaDuocDat); // ok
-App.post("/user/booking", userController.userBooking); // ok
+App.post("/user/booking",authentication, userController.userBooking); // ok
 
-App.get("/user/get/all/order", userController.getAllOrder); // ok
-App.post("/user/delete/order", userController.deleteOrder); // ok
-App.post("/user/update/seat", userController.editBooking); // ok
+App.get("/user/get/order", authentication, userController.getAllOrder); // ok
+App.post("/user/delete/order",authentication, userController.deleteOrder); // ok
+App.post("/user/update/seat", authentication, userController.editBooking); // ok
 
 /*Administrator*/
-App.post("/admin/add/movie", adminController.addMovie); // ok
-App.get("/admin/get/all/order", adminController.getAllOrder); // ok
-App.get("/admin/get/all/order/:date", adminController.getAllOrderByDate); // ok
-
-
-
-App.get('/', (req, res) => {
-  console.log('Inside the homepage callback function')
-  console.log(req.sessionID)
-  res.send(`You hit home page!\n`)
-})
-
-App.get('/`345`', (req, res) => {
-  if (req.isAuthenticated() && req.user.role === 'user') {
-
-    console.log('Inside the User homepage')
-    console.log(req.sessionID)
-    res.send(req.user)
-  } else {
-    res.send('you do not have permission')
-  }
-
-});
-
-App.get('/admin', (req, res) => {
-  console.log(req.user);
-  if (req.isAuthenticated() && req.user.role === 'admin') {
-    console.log('Inside the Admin homepage')
-    console.log(req.sessionID)
-    res.send(`You hit Admin page!\n`)
-  } else {
-    res.send('You do not have permission')
-  }
-});
+App.post("/admin/add/movie", authentication, adminController.addMovie); // ok
+App.get("/admin/get/all/order", authentication,adminController.getAllOrder); // ok
+App.get("/admin/get/all/order/:date", authentication, adminController.getAllOrderByDate); // ok
 
 App.get('/admin/login', (req, res) => {
   res.send('You Got Administrator login')
 })
 module.exports = { App };
-
-// this is test-auth branch
-// you can editable everything
--- server/src/controller/admin.js
+++ server/src/controller/admin.js
@@ -1,70 +1,60 @@
 const { pool } = require("../../src/mysql/connect");
 
 exports.addMovie = (req, res) => {
-  if (req.isAuthenticated() && req.user.role === 'admin') {
-    const { title, director, released, length, price, data, imageUrl } = req.body;
-    let dataString = JSON.stringify(data);
-    let sql = `call add_movie('${title}', '${director}', ${released}, ${length}, ${price}, '${imageUrl}','${dataString}');`;
-    try {
-      pool.query(sql, (error, results, feilds) => {
-        if (error) {
-          return res.status(400).send({ error });
-        }
-        if (results.affectedRows) {
-          res.status(200).send({
-            results: results,
-            query: { title, director, released, length, price, data }
-          });
-        } else {
-          res.status(400).send({
-            message: "Nothing to change."
-          });
-        }
-      });
-    } catch (error) {
-      res.status(400).send({ error });
-    }
-  } else {
-    res.redirect('/admin/login');
+  if (req.user.role !== 'admin') return res.send({ message: 'Please try to login as Admin' });
+  const { title, director, released, length, price, data, imageUrl } = req.body;
+  let sql = `call add_movie('${title}', '${director}', ${released}, ${length}, ${price}, '${imageUrl}','${dataString}');`;
+  try {
+    pool.query(sql, (error, results, feilds) => {
+      if (error) {
+        return res.status(400).send({ error });
+      }
+      if (results.affectedRows) {
+        res.status(200).send({
+          results: results,
+          query: { title, director, released, length, price, data }
+        });
+      } else {
+        res.status(400).send({
+          message: "Nothing to change."
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).send({ error });
   }
 };
 
 exports.getAllOrder = (req, res) => {
-  if (req.isAuthenticated() && req.user.role === 'admin') {
-    let sql = `call admin_getAllOrder();`;
-    try {
-      pool.query(sql, (error, results, feilds) => {
-        if (error) return res.status(400).send({ error });
-
-        let order = results[0];
-        res.status(200).send({
-          instance: order.length,
-          order: order
-        });
-      });
-    } catch (error) {
+  if (req.user.role !== 'admin') return res.send({ message: 'You do not have a permission. Please try login as Admin' });
+  let sql = `call admin_getAllOrder();`;
+  try {
+    pool.query(sql, (error, results, feilds) => {
       if (error) return res.status(400).send({ error });
-    }
-  } else {
-    res.redirect('/admin/login');
+
+      let order = results[0];
+      res.status(200).send({
+        instance: order.length,
+        order: order
+      });
+    });
+  } catch (error) {
+    if (error) return res.status(400).send({ error });
   }
 };
 
 exports.getAllOrderByDate = (req, res) => {
-  if (req.isAuthenticated() && req.user.role === 'admin') {
-    let date = req.params.date;
-    let sql = `call admin_getAllOrderByDate(${date});`;
-    try {
-      pool.query(sql, (error, results, feilds) => {
-        if (error) return res.status(400).send({ error });
-        let order = results[0];
-        res.status(200).send({
-          instance: order.length,
-          order: order
-        });
+  if (req.user.role !== 'admin') return res.send({ message: 'You do not have a permission. Please login as Admin' });
+  let date = req.params.date;
+  let sql = `call admin_getAllOrderByDate(${date});`;
+  try {
+    pool.query(sql, (error, results, feilds) => {
+      if (error) return res.status(400).send({ error });
+      let order = results[0];
+      res.status(200).send({
+        instance: order.length,
+        order: order
       });
-    } catch (error) { return res.status(400).send({ error }) }
-  } else {
-    res.redirect('/admin/login');
-  }
+    });
+  } catch (error) { return res.status(400).send({ error }) }
 };
