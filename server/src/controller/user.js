caa00747d027ed7f592f03bf73105ac82a47b592
lequangsang97 <sang.lequang97@gmail.com>
2018-11-07 16:38:03 +0700

    Fix respone user booking and delete token expire

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -70,8 +70,7 @@ exports.userPostLogin = (req, res) => {
               email: user.email,
               role: user.role
             },
-            "secure",
-            { expiresIn: "10m" }
+            "secure"
           );
           return res
             .status(200)
@@ -176,7 +175,7 @@ exports.getChoNgoiDaDuocDat = (req, res) => {
 exports.userBooking = (req, res) => {
   if (req.user.role !== "user")
     return res.send({ message: "Please login as customer" });
-  var { id_movie, id_date, id_time,id_seat } = req.body;
+  var { id_movie, id_date, id_time, id_seat } = req.body;
   var id_user = req.user.id_user;
   const seatedJson = JSON.stringify(id_seat);
   let sql = `call datVe(${id_user}, ${id_movie}, ${id_date}, ${id_time}, '${seatedJson}');`;
@@ -185,9 +184,12 @@ exports.userBooking = (req, res) => {
       if (error) {
         return res.status(400).send({ error });
       }
-      var order = results[0];
+      let orders = [];
+      for (order of results[0]) {
+        if (id_seat.includes(order.id_seat)) orders.push(order);
+      }
       res.status(200).send({
-        order: order
+        order: orders
       });
     });
   } catch (error) {
