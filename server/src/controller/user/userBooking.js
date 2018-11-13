d096251f65e1cc9bf6cbcb0e54a5e0097bf11ab8
unknown <sang.lequang97@gmail.com>
2018-11-13 22:08:30 +0700

    si

--- server/src/controller/user/userBooking.js
+++ server/src/controller/user/userBooking.js
@@ -1,26 +1,33 @@
 const { pool } = require("../../mysql/connect");
-
+const uuid = require("uuid/v4");
 exports.userBooking = (req, res) => {
-    if (req.user.role !== "user")
-      return res.send({ message: "Please login as customer" });
-    var { id_movie, id_date, id_time, id_seat } = req.body;
-    var id_user = req.user.id_user;
-    const seatedJson = JSON.stringify(id_seat);
-    let sql = `call datVe(${id_user}, ${id_movie}, ${id_date}, ${id_time}, '${seatedJson}');`;
-    try {
-      pool.query(sql, (error, results, fields) => {
-        if (error) {
-          return res.status(400).send({ error });
-        }
-        let orders = [];
-        for (order of results[0]) {
-          if (id_seat.includes(order.id_seat)) orders.push(order);
-        }
-        res.status(200).send({
-          order: orders
+  if (req.user.role !== "user")
+    return res.send({ message: "Please login as customer" });
+  var { id_movie, id_date, id_time, id_seat } = req.body;
+  var id_user = req.user.id_user;
+  const seatedJson = JSON.stringify(id_seat);
+
+  let orders = [];
+  const promise = new Promise((resolve, reject) => {
+    for (seat of id_seat) {
+      id_order = uuid();
+      let sql = `call datVe('${id_order}', ${id_user}, ${id_movie}, ${id_date}, ${id_time}, ${seat});`;
+      try {
+        pool.query(sql, (error, results, fields) => {
+          if (error) {
+            reject(error);
+          }
+          let data = JSON.stringify(results[0][0]);
+          order.push(data);
         });
-      });
-    } catch (error) {
-      res.status(400).send({ error });
+      } catch (error) {
+        reject(error);
+      }
     }
-  };
+  });
+
+  console.log(orders);
+  res.status(200).send({
+    order: orders
+  });
+};
--- server/src/controller/user/userPostCreateAccount.js
+++ server/src/controller/user/userPostCreateAccount.js
@@ -3,33 +3,34 @@ const bcrypt = require("bcrypt");
 
 exports.userPostCreateAccount = (req, res) => {
   let { email, password, name, sdt } = req.body;
-  let vError = {};
-  if (name && name.length < 2) {
-    vError.eName = "Name required 2 character.";
-  }
-  if (password && (password.length < 8 || password > 32)) {
-    vError.ePassword = "Password requied 8 character.";
-  }
-  if (sdt && (sdt.length < 10 || sdt.length > 15)) {
-    vError.ePhone = "PhoneNumber requied 10 character.";
-  }
-  if (Object.keys(vError).length) {
-    return res.status(400).json({ error: vError });
-  }
-
   if (email && password && name && sdt) {
+    let vError = {};
+    if (name.length < 2) {
+      vError.eName = "Name required at least 2 character.";
+    }
+    if (password.length < 8 || password > 32) {
+      vError.ePassword = "Password requied at least 8 character.";
+    }
+    if (sdt.match(/[a-z]/i) || sdt.length < 10 || sdt.length > 15) {
+      vError.ePhone = "PhoneNumber requied 10 character and not contain text";
+    }
+    if (Object.keys(vError).length) {
+      return res.status(400).json({ error: vError });
+    }
 
     name = name
       .split(/\s+/)
+      .map(val => val.charAt(0).toUpperCase() + val.substr(1).toLowerCase())
       .join(" ")
       .trim();
     email = email
       .split(/\s+/)
-      .join('')
-      .trim();
+      .join("")
+      .trim()
+      .toLowerCase();
     sdt = sdt
       .split(/\s+/)
-      .join('')
+      .join("")
       .trim();
 
     let passwordHash = password + "secure";
@@ -38,24 +39,28 @@ exports.userPostCreateAccount = (req, res) => {
       try {
         pool.query(sql, (error, results, fields) => {
           if (error) {
-            return res.status(400).send({
-              code: error.code,
-              errno: error.errno,
-              sqlMessage: error.sqlMessage,
-              sqlState: error.sqlState,
-              index: error.index
+            return res.status(400).json({
+              error: {
+                code: error.code,
+                errno: error.errno,
+                sqlMessage: error.sqlMessage,
+                sqlState: error.sqlState,
+                index: error.index
+              }
             });
           }
-          res.status(201).send({ name, email, sdt });
+          res.status(201).json({ user: { name, email, sdt } });
         });
       } catch (error) {
         res.status(400).send({ error });
       }
     });
   } else {
-    return res.status(400).send({
-      Message: "Require {name, email, password, sdt}",
-      YourBody: Object.keys(req.body)
+    return res.status(400).json({
+      error: {
+        Message: "Require {name, email, password, sdt}",
+        YourBody: Object.keys(req.body)
+      }
     });
   }
 };
