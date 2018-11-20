0b023dda840b07aa866c3c4c53edcdb1ca734d87
unknown <sang.lequang97@gmail.com>
2018-11-20 16:40:29 +0700

    final code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 const { App } = require("./app");
 const server = http.createServer(App);
-
+let count = 1;
 let port = process.env.PORT || 8080;
-server.on("error", (error) => console.log(error));
+server.on("error", error => console.log(error));
+
 server.listen(port, () => console.log(`Server running on ${port}`));
+
+server.on("connection", connect => {
+  let time = new Date().getTime();
+  console.log(
+    `>>>> Count Connection: ${count++} at ${new Date().toLocaleString()}`
+  );
+  //    console.log(new Date(time).toLocaleString());
+});
