936be7215e7385780117a713567c84e5689214dc
unknown <sang.lequang97@gmail.com>
2018-11-22 22:08:55 +0700

    Fix status of admin check out order

--- server/src/controller/admin/adminCheckOrderUser.js
+++ server/src/controller/admin/adminCheckOrderUser.js
@@ -2,33 +2,49 @@ const { pool } = require("../../mysql/connect");
 
 exports.adminCheckOrderUser = (req, res) => {
   let id_order = req.params.id_order;
-  let sql = `call checkOrderUser(${id_order});`;
-  try {
-    pool.query(sql, (error, results, feilds) => {
-      if (error) return res.status(400).send({ error });
-      let order = results[0];
-      if (order.length) {
-        return res.status(200).send({
-          user: {
-            name: order[0].name,
-            email: order[0].email,
-            phone_number: order[0].phone_number
-          },
-          order: {
-            title: order[0].title,
-            date: order[0].date,
-            time: order[0].time,
-            seat: order[0].id_seat
-          },
-          status: order[0].status,
-          message: "Success"
+  let checkoutPromise = new Promise((resolve, reject) => {
+    try {
+      let sql = `call checkOrderUser(${id_order});`;
+      pool.query(sql, (error, results, feilds) => {
+        if (error) return reject(error);
+        let order = results[0];
+        if (order.length) {
+          return resolve({
+            user: {
+              name: order[0].name,
+              email: order[0].email,
+              phone_number: order[0].phone_number
+            },
+            order: {
+              title: order[0].title,
+              date: order[0].date,
+              time: order[0].time,
+              seat: order[0].id_seat
+            },
+            status: order[0].status,
+            message: "Success"
+          });
+        }
+        return reject({
+          error: "Can not found!"
         });
-      }
-      res.status(404).send({
-        error: "Can not found!"
       });
-    });
-  } catch (error) {
-    return res.status(400).send({ error });
-  }
+    } catch (error) {
+      return reject({ error });
+    }
+  }).then((data) => {
+    let sql = `update reservation
+               set status = 'processed'
+               where id_order = ${id_order}`
+    try {
+      pool.query(sql, (error, results, feilds) => {
+        data.status = 'processed';
+        return res.send(data);
+      });
+    } catch (error) {
+      return res.send({error})
+    }
+  }, (error) => {
+    return res.send({error})
+  });
 };
