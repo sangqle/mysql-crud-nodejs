162ec8f0b8335e81347e17952eef5ce246b9a54d
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2018-11-30 14:10:08 +0000

    for the last step

--- client/src/components/editMovie.js
+++ client/src/components/editMovie.js
@@ -1,12 +1,118 @@
 import React from "react";
+import { Formik } from "formik";
 
-const EditMovie = props => {
-  console.log(props.movie);
-  return (
-    <div>
-      <h1>Edit Pqge</h1>
-    </div>
-  );
-};
+class EditPage extends React.Component {
+  state = {
+    movie: null
+  };
 
-export default EditMovie;
+  componentWillMount() {
+    console.log(this.props.id_movie);
+    fetch(`http://localhost:8080/admin/get/movie/${this.props.id_movie}`, {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "x-auth": localStorage.getItem("token"),
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ movie: data.movie });
+      });
+  }
+
+  render() {
+    const { movie } = this.state;
+    console.log(this.state);
+    return (
+      movie && (
+        <div>
+          <h1>Edit your movie now !</h1>
+          <Formik
+            initialValues={{
+              director: movie.director,
+              discription: movie.discription,
+              image: movie.image,
+              length: movie.length,
+              price: movie.price,
+              released: movie.released,
+              title: movie.title
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  type="text"
+                  name="title"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.title}
+                />
+                <label>Director</label>
+                <input
+                  type="text"
+                  name="director"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.director}
+                />
+                <label>Description</label>
+                <input
+                  type="text"
+                  name="description"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.discription}
+                />
+                <label>Image</label>
+                <input
+                  type="text"
+                  name="image"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.image}
+                />
+                <label>Length</label>
+                <input
+                  type="text"
+                  name="length"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.length}
+                />
+                <label>Year released</label>
+                <input
+                  type="text"
+                  name="released"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.released}
+                />
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      )
+    );
+  }
+}
+
+export default EditPage;
--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import PageNotFound from "../src/components/404";
 import Order from "../src/container/order";
 import VerifyOrder from "../src/container/orderSuccess";
 import AddMovie from "../src/container/addmovie";
-import ViewOrder from './container/adminViewOrders'
+import ViewOrder from "./container/adminViewOrders";
 import User from "../src/components/user";
 import EditPage from "./components/editMovie";
 //import App from "../src/components/OrderPage/App";
@@ -29,7 +29,7 @@ const Page = () => {
       <Order path="order/:id_movie" />
       <VerifyOrder path="order/success/:id_order" />
       <AddMovie path="admin/add_movie" />
-      <EditPage path="admin/edit_movie/:movie" />
+      <EditPage path="admin/edit_movie/:id_movie" />
       <ViewOrder path="admin/view_ordered" />
       <PageNotFound path="*" />
     </Router>
